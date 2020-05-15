d22e6b30a2869d9475a8cbc5bac93f9769397c45
Breno Baronte <brenobaronte@hotmail.com>
2020-05-14 23:53:57 -0300

    register with email and password

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -13,10 +13,12 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
 
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   // text field state
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -39,10 +41,12 @@ class _RegisterState extends State<Register> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: <Widget>[
               SizedBox(height: 20.0),
               TextFormField(
+                validator: (value) => value.isEmpty ? 'Enter an email' : null,
                 onChanged: (value) {
                   setState(() => email = value);
                 },
@@ -50,6 +54,8 @@ class _RegisterState extends State<Register> {
               SizedBox(height: 20.0),
               TextFormField(
                 obscureText: true,
+                validator: (value) => value.length < 6
+                    ? 'Enter a password 6+ chars long' : null,
                 onChanged: (value) {
                   setState(() => password = value);
                 },
@@ -64,10 +70,21 @@ class _RegisterState extends State<Register> {
                   ),
                 ),
                 onPressed: () async {
-                  print(email);
-                  print(password);
+                  if (_formKey.currentState.validate()) {
+                    dynamic result =
+                      await _auth.registerWithEmailAndPassword(email, password);
+                    if (result == null){
+                      print('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee');
+                      setState(() => error = 'plase supply a valid email');
+                    }
+                  }
                 },
               ),
+              SizedBox(height: 12.0),
+              Text(
+                error,
+                style: TextStyle(color: Colors.red, fontSize: 14.0),
+              )
             ],
           ),
         ),
